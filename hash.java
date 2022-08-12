8f32e9869ed27211c6a26586753f65c260136069
Keerthana-P0310 <111142502+Keerthana-P0310@users.noreply.github.com>
2022-08-12 23:06:09 +0530

    Add files via upload

---
+++ hash.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+public class hash{
+    public static void main(String[] args) {
+        Set<Integer> numbers = new HashSet<>();
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(10);
+
+        System.out.println("numbers : " + numbers);}
+		}
